c8d804d45bad76d063d954e54b59176507108da5
Tewan <s6652410012@sau.ac.th>
2026-02-27 16:05:53 +0700

    9.1

--- app/detail.tsx
+++ app/detail.tsx
@@ -1,12 +1,38 @@
+import * as Linking from "expo-linking";
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
-export default function detail() {
-  return (
-    <View>
-      <Text>detail</Text>
-    </View>
-  );
+export default function Detail() {
+  //สร้างตัวแปรเก็บข้อมูลที่ส่งมาจากหน้า Home
+  const params = useLocalSearchParams();
+
+  //ฟังก์ชันเพื่อเปิดแอปโทรศัพท์
+  const openPhone = () => {
+    const phoneNumber = params.phone as string;
+    const url = `tel:${phoneNumber}`;
+    Linking.openURL(url);
+  };
+
+  // ฟังก์ชันสำหรับเปิดแอปแผนที่
+  const handleOpenMapApp = () => {
+    //สร้างตัวแปรเพื่อเปิด Google Maps
+    const googleMap = `https://maps.google.com/?q=${params.latitude},${params.longitude}`;
+
+    //สร้างตัวแปรเพื่อเปิด Apple Maps
+    const appleMap = `http://maps.apple.com/?q=${params.name}?&ll=${params.latitude},${params.longitude}`;
+
+    //ตรวจสอบการเปิดแอป Google Maps หรือ Apple Maps โดยยึด Google Maps เป็นหลัก
+    Linking.canOpenURL(googleMap).then((supported) => {
+      if (supported) {
+        Linking.openURL(googleMap);
+      } else {
+        Linking.openURL(appleMap);
+      }
+    });
+  };
+
+  return <ScrollView style={{ flex: 1 }}></ScrollView>;
 }
 
 const styles = StyleSheet.create({});
